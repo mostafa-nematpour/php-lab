a1c37af18def83c483a2894c85fb88f4d4a00087
Mostafa <nematpor46@gmail.com>
2021-11-13 11:58:16 +0330

    implementation response class

--- php-lab-simple/5.api/api/v1/cities/index.php
+++ php-lab-simple/5.api/api/v1/cities/index.php
@@ -1,11 +1,10 @@
 <?php
-
-
 include "../../../loader.php";
+
 use \App\Services\CityService;
 use \App\Utilities\Response;
 
 $cs = new CityService();
 $result = $cs->getCities((object)[1, 1, 2, 1]);
 
-echo Response::respond($result, 200);
+Response::respondAndDie($result, Response::HTTP_OK);
