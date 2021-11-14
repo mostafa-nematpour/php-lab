a0d842fe71da78de22e17c2af2c3f80e1f53dc14
Mostafa <nematpor46@gmail.com>
2021-11-14 21:12:52 +0330

    add crud in api

--- php-lab-simple/5.api/App/Services/CityService.php
+++ php-lab-simple/5.api/App/Services/CityService.php
@@ -1,8 +1,30 @@
 <?php
+
 namespace App\Services;
-class CityService {
+
+class CityService
+{
     public function getCities($data)
     {
-        return $data;
+        $result = getCities($data);
+        return $result;
+    }
+
+    public function createCity($data)
+    {
+        $result = addCity($data);
+        return $result;
+    }
+
+    public function updateCityName($city_id, $name)
+    {
+        $result = changeCityName($city_id, $name);
+        return $result;
+    }
+
+    public function deleteCity($city_id)
+    {
+        $result = deleteCity($city_id);
+        return $result;
     }
 }
--- php-lab-simple/5.api/api/v1/cities/index.php
+++ php-lab-simple/5.api/api/v1/cities/index.php
@@ -4,7 +4,66 @@ include "../../../loader.php";
 use \App\Services\CityService;
 use \App\Utilities\Response;
 
-$cs = new CityService();
-$result = $cs->getCities((object)[1, 1, 2, 1]);
 
-Response::respondAndDie($result, Response::HTTP_OK);
+$request_method = $_SERVER['REQUEST_METHOD'];
+
+$request_body = json_decode(file_get_contents('php://input'), true);
+
+$city_service = new CityService();
+
+// Response::respondAndDie($request_body);
+switch ($request_method) {
+
+    case 'GET':
+        $province_id = $_GET['province_id'] ?? null;
+
+
+        # do validate : province_id
+        $request_data = [
+            'province_id' => $province_id,
+            'fields' => $_GET['fields'] ?? null,
+            'page' => $_GET['page'] ?? null,
+            'pagesize' => $_GET['pagesize'] ?? null,
+        ];
+
+        $response = $city_service->getCities($request_data);
+
+        if (empty($response))
+            Response::respondAndDie($response, Response::HTTP_NOT_FOUND);
+
+        Response::respondAndDie($response, Response::HTTP_OK);
+
+
+    case 'POST':
+        if (!isValidCity($request_body))
+            Response::respondAndDie(["invalid City data"], Response::HTTP_NOT_ACCEPTABLE);
+        $response = $city_service->createCity($request_body);
+        Response::respondAndDie($response, Response::HTTP_CREATED);
+
+
+    case 'PUT':
+        [$city_id, $city_name] = [$request_body['city_id'], $request_body['name']];
+        if (!is_numeric($city_id) or empty($city_name)) {
+            Response::respondAndDie(["invalid City data"], Response::HTTP_NOT_ACCEPTABLE);
+        }
+        $response = $city_service->updateCityName($city_id, $city_name);
+        // if ($) {
+        //     # code...
+        // }
+        Response::respondAndDie($response, Response::HTTP_OK);
+
+
+    case 'DELETE':
+
+        $city_id = $_GET['city_id'];
+        if (!is_numeric($city_id) or is_null($city_id)) {
+            Response::respondAndDie(["invalid City data"], Response::HTTP_NOT_ACCEPTABLE);
+        }
+        $response = $city_service->deleteCity($city_id);
+
+        Response::respondAndDie($response, Response::HTTP_OK);
+
+
+    default:
+        Response::respondAndDie(['Invalid request method'], Response::HTTP_METHOD_NOT_ALLOWED);
+}
